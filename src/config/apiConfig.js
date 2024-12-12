d10c15ee3fdd798d5ca37a9a9f4e51b64a349e81
Justas <justas.valutis@student.vives.be>
2024-12-13 00:45:41 +0100

    before expo upgrade

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8090/api/';
+// export const BASE_URL = 'http://localhost:8090/api/';
+export const BASE_URL = 'https://deployment-subscription.onrender.com/api/';
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userId, setUserId] = useState(0);
   const [isAdmin, setIsAdmin] = useState(false);
 
